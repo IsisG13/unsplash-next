68c88bd25ec8f26f2220361b3c304043a48d3ee3
Isis <isissouzaguimaraes@gmail.com>
2024-05-07 17:51:56 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = {
-  env: {
-    UNSPLASH_aCCESS_KEY: process.env.UNSPLASH_ACCESS_KEY,
-  }
 };
 
 export default nextConfig;
